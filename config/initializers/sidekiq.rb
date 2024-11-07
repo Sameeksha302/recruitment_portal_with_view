d344fe42e9a0e23f1f6163f1f184b8f136a1a3c8
= <=>
2024-11-07 23:48:07 +0530

    sidekiq.rb schdeduler added

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,10 @@
+require "sidekiq-scheduler"
+
 Sidekiq.configure_server do |config|
     config.redis = { url: "redis://localhost:6379/0" }
+    config.on(:startup) do
+        Sidekiq::Scheduler.load_schedule!
+    end
 end
 
 Sidekiq.configure_client do |config|
