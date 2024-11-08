e7f3c0f19e8ade9c7d07b486d704c5fb1a70bfb4
= <=>
2024-11-08 15:35:21 +0530

    config.active_job.queue_adapter = :sidekiq in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,7 @@ Rails.application.configure do
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
   config.action_mailer.perform_caching = false
+  config.active_job.queue_adapter = :sidekiq
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
