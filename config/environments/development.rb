38006ad39b28f7e19213b780b6ff7939774c422c
Sameeksha Singh <96697486+Sameeksha302@users.noreply.github.com>
2024-11-29 13:29:27 +0530

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "gmail.com",
+    domain: "recruitment-portal-with-view.onrender.com",
     user_name: "sameekshasingh951@gmail.com", # Use "apikey" as the username
     password: "pehr rkgp gidy uraj", # Rails.application.credentials.dig(:sendgrid, :api_key)
     authentication: :plain,
