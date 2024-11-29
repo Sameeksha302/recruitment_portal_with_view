7e47db602477642387c722edd93f8aecc8b2ae4a
Sameeksha Singh <96697486+Sameeksha302@users.noreply.github.com>
2024-11-29 14:54:54 +0530

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,8 +41,8 @@ Rails.application.configure do
   # caching is enabled.
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
-  # config.action_mailer.default_url_options = { host: "recruitment-portal-p68x.onrender.com", port: "https" }
+  # config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.default_url_options = { host: "recruitment-portal-with-view.onrender.com", port: "https" }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
